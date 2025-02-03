c065d0bd439bf1b89a473fe22a006540bea3d401
Utkarsh-puranik <puranikutkarsh@gmail.com>
2025-02-03 11:37:10 +0530

    Update Logical operator.py

---
+++ Operators/Logical operator.py
@@ -0,0 +1,21 @@
+# Logical AND
+x = 5
+y = 10
+z = 15
+
+# Check if both conditions are true
+if x < y and y < z:
+    print("Both conditions are true (x < y and y < z)")
+
+# Logical OR
+if x > y or y < z:
+    print("At least one condition is true (x > y or y < z)")
+
+# Logical NOT
+is_raining = True
+
+# Negate the boolean value
+if not is_raining:
+    print("It's not raining")
+else:
+    print("It is raining")
